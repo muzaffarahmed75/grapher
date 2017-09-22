19ee5a2bb4cd34fcdb4f829ffbb8d9f55acd3db6
Muzaffar Ahmed <muzaffarahmed75@gmail.com>
2017-09-22 16:02:22 +0530

    Third Commit

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
+from __future__ import division
 from tkinter import *
 from math import log, sqrt, e
-
 
 class Grid:
     def __init__(self, master, width, height, step, *typ):
@@ -10,7 +10,7 @@ class Grid:
         self.step = step
         if typ:
             self.typ = typ[0]
-        # the variable typ refers to the type of graph, i.e. 0 for regular,
+        # The variable typ refers to the type of graph, i.e. 0 for regular,
         # 1 for semi-log along x, 2 for semi-log along y, 3 for log-log
         graph = Canvas(master, width=width, height=height, bg='white')
 
@@ -28,7 +28,7 @@ class Grid:
                 graph.create_line(0, i, width, i, fill='#999999')
 
             divs = typ[1]
-            for i in range(0, width, width/divs):
+            for i in range(0, width, width//divs):
                 for j in range(1, 10):
                     k = i + width*(log(j, 10))/divs
                     graph.create_line(k, 0, k, height, fill='#999999')
@@ -38,19 +38,19 @@ class Grid:
                 graph.create_line(i, 0, i, height, fill='#999999')
 
             divs = typ[1]
-            for i in range(0, height, height/divs):
+            for i in range(0, height, height//divs):
                 for j in range(1, 10):
                     k = height - i - height*(log(j, 10))/divs
                     graph.create_line(0, k, width, k, fill='#999999')
 
         elif typ[0] == 3:
-            for i in range(0, height, height/step):
+            for i in range(0, height, height//step):
                 for j in range(1, 10):
                     k = height - i - height*(log(j, 10))/step
                     graph.create_line(0, k, width, k, fill='#999999')
 
             divs = typ[1]
-            for i in range(0, width, width/divs):
+            for i in range(0, width, width//divs):
                 for j in range(1, 10):
                     k = i + width*(log(j, 10))/divs
                     graph.create_line(k, 0, k, height, fill='#999999')
@@ -66,6 +66,7 @@ class Grid:
         graph.pack()
 
 
+# This class will be used for inputting number of points to be plotted.
 class Form1:
     def __init__(self, master):
         def save():
@@ -75,7 +76,7 @@ class Form1:
         Label(master, text="Number of points").grid(row=0)
         e1 = Entry(master)
         e1.grid(row=0, column=1)
-        Button(master, text='OK', command=save).grid(row=3, column=1, sticky=W, pady=4)
+        Button(master, text='OK', command=save).grid(row=3, columnspan=2, pady=4)
 
 
 while 1:
@@ -90,22 +91,51 @@ while 1:
     break
 
 
+# This class will be used for inputting the points.
 class Form2:
     def __init__(self, master):
+        def save():
+            self.data1 = [float(j.get()) for j in ela]
+            self.data2 = [float(j.get()) for j in elb]
+            master.quit()
         Label(master, text="Quantity A").grid(row=0)
         Label(master, text="Quantity B").grid(row=0, column=1)
-        ela,elb = [None]*n,[None]*n
+        ela, elb = [None]*n, [None]*n
         for i in range(n):
-            ela[i] = Entry(master)
+            ela[i] = Entry(master, borderwidth=1)
             ela[i].grid(row=i+1)
         for i in range(n):
-            elb[i] = Entry(master)
+            elb[i] = Entry(master, borderwidth=1)
             elb[i].grid(row=i+1, column=1)
+        Button(master, text='Continue', command=save).grid(row=n+2, columnspan=2, pady=4)
+        ty = StringVar()
+        ty.set("one")
+        #w = OptionMenu(master, ty, "one", "two", "three")
+        #w.pack()
 
 
-root2 = Tk()
-form2 = Form2(root2)
-root2.mainloop()
+while 1:
+    try:
+        root2 = Tk()
+        form2 = Form2(root2)
+        root2.mainloop()
+        root2.destroy()
+        xl = form2.data1
+        yl = form2.data2
+        xm = sum(xl)/n
+        ym = sum(yl)/n
+        m = sum([(xl[i]-xm)*yl[i] for i in range(n)]) / sum([(xl[i]-xm)**2 for i in range(n)])
+        c = ym - m*xm
+        if c*69 < max(yl) - min(yl):
+            isk = False
+        else:
+            isk = True
+        allscales = [a*10**b for a in [1.0, 2.0, 2.5, 4.0, 5.0] for b in range(-15, 15)]
+
+    except:
+        continue
+    break
+
 
 
 root3 = Tk()
